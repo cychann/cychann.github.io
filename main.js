880eaf53676fc553fa9423398adb510903494a31
cychann <skcbdlhc129@naver.com>
2021-04-09 21:06:57 +0900

    final

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 const myImage = document.querySelector('.my_image')
+const myInfo = document.querySelector('.my_info')
+const clickMent = document.querySelector('.click_ment')
+infoHide = true
 
 myImage.addEventListener('click', () => {
-    console.log("asd")
-    myImage.style
+    infoHideOrShow()
 })
+
+// navbar 메뉴 클릭 시 이동
+const navbarMenu = document.querySelector('.navbar_menu')
+navbarMenu.addEventListener('click', (event) => {
+    const link = event.target.dataset.link
+    const scrollTo = document.querySelector(`.${link}`)
+
+    scrollTo.scrollIntoView({behavior: 'smooth'})
+})
+
+function infoHideOrShow() {
+    if(infoHide == true) {
+        myInfo.classList.remove('hide')
+        infoHide = false
+        clickMent.classList.add('hide')
+    } 
+    else {
+        myInfo.classList.add('hide')
+        infoHide = true
+        clickMent.classList.remove('hide')
+    }
+}
